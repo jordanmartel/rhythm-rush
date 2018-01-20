5c4cdf9d4da8a854ff74b3565810ef308841d112
stoupare <alex.stouparenko@mail.utoronto.ca>
2018-01-20 13:46:48 -0500

    react and destroy notes past location/on input

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour {
 
-	private void checkControlDestroyer ()  {
-			
+	public NoteDestroyer p1NoteDestroyer;
+	public NoteDestroyer p2NoteDestroyer;
+
+	private void checkControlDestroyer (KeyCode key, NoteDestroyer destroyer)  {
+
+		if (destroyer != null) {
+			List<GameObject> notes = destroyer.getNotes ();
+			foreach (GameObject note in notes) {
+				Destroy (note);
+				destroyer.incrementScore (100);
+			}
+		}
 	}
 
 
 	// Use this for initialization
 	void Start () {
-		
-
 	}
 	
 	// Update is called once per frame
@@ -20,31 +28,31 @@ public class InputController : MonoBehaviour {
 
 		// Player1 Input 
 		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-			
+			checkControlDestroyer (KeyCode.LeftArrow, p1NoteDestroyer);
 		}
 		else if (Input.GetKeyDown(KeyCode.RightArrow)) {
-			
+			checkControlDestroyer (KeyCode.RightArrow, p1NoteDestroyer);
 		}
 		else if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			
+			checkControlDestroyer (KeyCode.UpArrow, p1NoteDestroyer);
 		}
 		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			
+			checkControlDestroyer (KeyCode.DownArrow, p1NoteDestroyer);
 		}
 
 
 		//Player 2 Input  
 		else if (Input.GetKeyDown(KeyCode.A)) {
-
+			checkControlDestroyer (KeyCode.A, p2NoteDestroyer);
 		}
 		else if (Input.GetKeyDown(KeyCode.W)) {
-
+			checkControlDestroyer (KeyCode.W, p2NoteDestroyer);
 		}
 		else if (Input.GetKeyDown(KeyCode.D)) {
-
+			checkControlDestroyer (KeyCode.D, p2NoteDestroyer);
 		}
 		else if (Input.GetKeyDown(KeyCode.S)) {
-
+			checkControlDestroyer (KeyCode.S, p2NoteDestroyer);
 		}
 
 
--- Assets/Scripts/NoteDestroyer.cs
+++ Assets/Scripts/NoteDestroyer.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class NoteDestroyer : MonoBehaviour {
 
 	private int score;
+	private List<GameObject> colliderNotes;
 
-	void OnTriggerExit (Collider other) {
-		Destroy (other.gameObject);
+	public List<GameObject> getNotes(){
+		return colliderNotes;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void incrementScore(int inc) {
+		score += inc;
+	}
+
+	void OnTriggerExit (Collider note) {
+		Destroy (note.gameObject);
+	}
+
+	void OnTriggerEnter (Collider note) {
+		colliderNotes.Add(note.gameObject);
 	}
 
 	// Use this for initialization
 	void Start () {
 		score = 0;
+		colliderNotes = new List<GameObject> ();
 	}
 	
 	// Update is called once per frame
