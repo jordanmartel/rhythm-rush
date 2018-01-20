ccef5954fb37ac72fe3997582c21151e03d41722
Jordan Martel <jfm_martel@hotmail.com>
2018-01-20 14:00:29 -0500

    beatmap generation from JSON file

---
+++ Assets/Beatmap.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class Beatmap
+{
+
+    public Note[] notes;
+
+    // go through all the notes and find the next note that has not happened yet
+    Note GetNextNote(float time)
+    {
+        for (int i = 0; i < notes.Length; i ++) {
+
+            if (notes[i].time > time)
+            {
+                return notes[i];
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/Note.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class Note
+{
+
+    public float time;
+    public string button;
+
+}
