a97982a858a2f1efecb2aa17d24acf14b597051f
stoupare <alex.stouparenko@mail.utoronto.ca>
2018-01-20 12:52:47 -0500

    added collider object that detects notes and destroys them

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour {
 
+	private void checkControlDestroyer ()  {
+			
+	}
+
+
 	// Use this for initialization
 	void Start () {
 		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		// Player1 Input 
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			
+		}
+		else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			
+		}
+
+
+		//Player 2 Input  
+		else if (Input.GetKeyDown(KeyCode.A)) {
+
+		}
+		else if (Input.GetKeyDown(KeyCode.W)) {
+
+		}
+		else if (Input.GetKeyDown(KeyCode.D)) {
+
+		}
+		else if (Input.GetKeyDown(KeyCode.S)) {
+
+		}
+
+
 	}
 }
---
+++ Assets/Scripts/NoteDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteDestroyer : MonoBehaviour {
+
+	private int score;
+
+	void OnTriggerExit (Collider other) {
+		Destroy (other.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
