57d5c179954a59e0d9db746ed19a5691c33def21
MayChant <annied617@gmail.com>
2018-01-21 23:59:28 -0500

    demo stage fixed

--- Assets/Scripts/NoteMovement.cs
+++ Assets/Scripts/NoteMovement.cs
@@ -2,24 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoteMovement : MonoBehaviour {
+public class NoteMovement : MonoBehaviour
+{
 
-	public float speed = 3.0f;
-	public string direction;
-    
+    public float speed = 3.0f;
+    public string direction;
+
     // Use this for initialization
 
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
 
-		if (direction == "left") {
-			transform.Translate (Vector3.left * Time.deltaTime * speed);
-		}
-		else if (direction == "right") {
-			transform.Translate (Vector3.right * Time.deltaTime * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (direction == "left")
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        else if (direction == "right")
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
     }
 }
