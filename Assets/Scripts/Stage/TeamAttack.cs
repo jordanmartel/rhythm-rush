b0f1be321ad2cdbdef4815f2d280cfe1c6ad32ec
MayChant <annied617@gmail.com>
2018-02-05 10:13:49 -0500

    new files

---
+++ Assets/Scripts/Stage/TeamAttack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeamAttack : MonoBehaviour {
+    public int combo;
+    public int powerLvl;
+    public int powerLvlRequirement = 10;
+    public int attackDamage = 1000;
+    public float timer = 0f;
+    public Slider energyBar;
+
+	// Use this for initialization
+	void Start () {
+        energyBar.maxValue = powerLvlRequirement;
+        energyBar.gameObject.SetActive(false);
+	}
+
+    internal int buildTeamAttack()
+    {
+        energyBar.gameObject.SetActive(true);
+        GetComponentInChildren<Text>().text = "Mash the buttons!";
+        powerLvl++;
+        if (powerLvl >= powerLvlRequirement)
+        {
+            return unleashTeamAttack();
+        }
+        return 0;
+    }
+
+    internal int unleashTeamAttack()
+    {
+        int damageDone = attackDamage;
+        return damageDone;
+    }
+
+    internal void Reset()
+    {
+        powerLvl = 0;
+        powerLvlRequirement += 10;
+        attackDamage += 1200;
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponentInChildren<Text>().text = combo.ToString();
+        energyBar.value = Mathf.Min(powerLvl,powerLvlRequirement);
+	}
+}
