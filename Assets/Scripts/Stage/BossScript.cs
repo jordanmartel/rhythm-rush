9802925a38d3709183f497111586503724d855fe
MayChant <annied617@gmail.com>
2018-02-12 22:40:06 -0500

    add winning scene with ranks

--- Assets/Scripts/Stage/BossScript.cs
+++ Assets/Scripts/Stage/BossScript.cs
@@ -7,9 +7,9 @@ public class BossScript : MonoBehaviour {
 
     public int maxhp = 100000;
     public int dmg = 0;
-    public int endStatus = 0;
     private int hp;
     public Scrollbar healthBar;
+    public Canvas winning, ssRank, sRank, aRank, bRank, cRank, dRank;
   
 	// Use this for initialization
 	void Start () {
@@ -23,7 +23,30 @@ public class BossScript : MonoBehaviour {
 		if (hp <= 0)
         {
             //Winning
-            endStatus = 1;
+            Instantiate(winning, Vector3.zero, Quaternion.identity);
+            Ranking ranking = GameObject.FindObjectOfType<Ranking>();
+            string rank = ranking.rankingAtTime(ranking.time);
+            switch (rank)
+            {
+                case "SS":
+                    Instantiate(ssRank, Vector3.zero, Quaternion.identity);
+                    break;
+                case "S":
+                    Instantiate(sRank, Vector3.zero, Quaternion.identity);
+                    break;
+                case "A":
+                    Instantiate(aRank, Vector3.zero, Quaternion.identity);
+                    break;
+                case "B":
+                    Instantiate(bRank, Vector3.zero, Quaternion.identity);
+                    break;
+                case "C":
+                    Instantiate(cRank, Vector3.zero, Quaternion.identity);
+                    break;
+                default:
+                    Instantiate(dRank, Vector3.zero, Quaternion.identity);
+                    break;
+            }
         }
         
         // if player health <= 0, endStatus = -1
