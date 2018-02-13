ac833efd0b2afd171030e33527b81bc9cae3f8cf
MayChant <annied617@gmail.com>
2018-02-12 22:46:54 -0500

    not sure what I changed

--- Assets/Scripts/Stage/BossScript.cs
+++ Assets/Scripts/Stage/BossScript.cs
@@ -10,7 +10,8 @@ public class BossScript : MonoBehaviour {
     private int hp;
     public Scrollbar healthBar;
     public Canvas winning, ssRank, sRank, aRank, bRank, cRank, dRank;
-  
+
+    public bool hasEnded;
 	// Use this for initialization
 	void Start () {
         hp = maxhp;
@@ -18,36 +19,40 @@ public class BossScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        //Update visual cue on boss
-		if (hp <= 0)
+        if (!hasEnded)
         {
-            //Winning
-            Instantiate(winning, Vector3.zero, Quaternion.identity);
-            Ranking ranking = GameObject.FindObjectOfType<Ranking>();
-            string rank = ranking.rankingAtTime(ranking.time);
-            switch (rank)
+            //Update visual cue on boss
+            if (hp <= 0)
             {
-                case "SS":
-                    Instantiate(ssRank, Vector3.zero, Quaternion.identity);
-                    break;
-                case "S":
-                    Instantiate(sRank, Vector3.zero, Quaternion.identity);
-                    break;
-                case "A":
-                    Instantiate(aRank, Vector3.zero, Quaternion.identity);
-                    break;
-                case "B":
-                    Instantiate(bRank, Vector3.zero, Quaternion.identity);
-                    break;
-                case "C":
-                    Instantiate(cRank, Vector3.zero, Quaternion.identity);
-                    break;
-                default:
-                    Instantiate(dRank, Vector3.zero, Quaternion.identity);
-                    break;
+                //Winning
+                Instantiate(winning, Vector3.zero, Quaternion.identity);
+                Ranking ranking = GameObject.FindObjectOfType<Ranking>();
+                string rank = ranking.rankingAtTime(ranking.time);
+                switch (rank)
+                {
+                    case "SS":
+                        Instantiate(ssRank, Vector3.zero, Quaternion.identity);
+                        break;
+                    case "S":
+                        Instantiate(sRank, Vector3.zero, Quaternion.identity);
+                        break;
+                    case "A":
+                        Instantiate(aRank, Vector3.zero, Quaternion.identity);
+                        break;
+                    case "B":
+                        Instantiate(bRank, Vector3.zero, Quaternion.identity);
+                        break;
+                    case "C":
+                        Instantiate(cRank, Vector3.zero, Quaternion.identity);
+                        break;
+                    default:
+                        Instantiate(dRank, Vector3.zero, Quaternion.identity);
+                        break;
+                }
+                hasEnded = true;
             }
         }
+
         
         // if player health <= 0, endStatus = -1
         // then if endStatus != 0, wait until the end of the current attack/damage animations are done
