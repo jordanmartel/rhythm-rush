3350431d2702533b851f4cc927aefb4303333ffd
MayChant <annied617@gmail.com>
2018-03-16 22:15:39 -0400

    text effect

--- Assets/Scripts/Stage/PlayerStats.cs
+++ Assets/Scripts/Stage/PlayerStats.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour {
     public int maxCombo;
@@ -10,15 +11,22 @@ public class PlayerStats : MonoBehaviour {
     public int reviveCount;
     public int score;
 
-	// Use this for initialization
-	void Start () {
+    public Text scoreText;
+    public Text comboText;
+    public Vector3 comboInitial;
+    public Vector3 scoreInitial;
+
+    // Use this for initialization
+    void Start () {
         maxCombo = 0;
         failCount = 0;
         perfectCount = 0;
         stunCount = 0;
         reviveCount = 0;
         score = 0;
-	}
+        comboInitial = comboText.transform.localScale;
+        scoreInitial = scoreText.transform.localScale;
+}
 	
     internal void updateMaxCombo(int combo)
     {
@@ -26,10 +34,13 @@ public class PlayerStats : MonoBehaviour {
         {
             maxCombo = combo;
         }
+        StartCoroutine("comboTextJump");
     }
 
     internal void updateScore(int score) {
+        print("update score");
         this.score += score;
+        StartCoroutine("scoreTextJump");
     }
 
     internal void incrementFail()
@@ -50,6 +61,30 @@ public class PlayerStats : MonoBehaviour {
     internal void incrementRevive()
     {
         reviveCount++;
+    }
+
+    IEnumerator scoreTextJump()
+    {
+        float multiplier = 1.4f;
+        scoreText.transform.localScale = new Vector3(scoreInitial.x * multiplier, scoreInitial.y * multiplier, scoreInitial.z * multiplier);
+        while (Mathf.Abs(multiplier - 1f) > 1e-3)
+        {
+            multiplier -= 0.05f;
+            scoreText.transform.localScale = new Vector3(scoreInitial.x * multiplier, scoreInitial.y * multiplier, scoreInitial.z * multiplier);
+            yield return null;
+        }
+    }
+
+    IEnumerator comboTextJump()
+    {
+        float multiplier = 1.4f;
+        comboText.transform.localScale = new Vector3(comboInitial.x * multiplier, comboInitial.y * multiplier, comboInitial.z * multiplier);
+        while (Mathf.Abs(multiplier - 1f) > 1e-3)
+        {
+            multiplier -= 0.05f;
+            comboText.transform.localScale = new Vector3(comboInitial.x * multiplier, comboInitial.y * multiplier, comboInitial.z * multiplier);
+            yield return null;
+        }
     }
 
 	// Update is called once per frame
