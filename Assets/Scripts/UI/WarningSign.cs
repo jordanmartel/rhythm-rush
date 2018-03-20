5fc3da4547539ff263068a28b824ce22d5282a90
MayChant <annied617@gmail.com>
2018-03-20 19:46:42 -0400

    add warning sign; change miss sfx because the old one blends in too well with water level mumsic

---
+++ Assets/Scripts/UI/WarningSign.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarningSign : MonoBehaviour {
+    private double timer;
+    private const double appearTime = 0.5;
+    private const double disappearTime = 1;
+    private const int maxFlashTime = 3;
+    private int flashTime = 0;
+    public Image sign;
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        flashTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= disappearTime)
+        {
+            sign.enabled = true;
+            timer = 0;
+            flashTime++;
+            
+        }
+        else if (timer >= appearTime)
+        {
+            sign.enabled = false;
+        }
+        if (flashTime >= maxFlashTime)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
